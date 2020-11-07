8d4275936981f350dbd645a3dd2eb991c218156f
rodrigorsf <rodrigo.freitas95@academico.ifs.edu.br>
2020-11-07 19:57:56 -0300

    Adding customTextBox with different color on focus

---
+++ DemoCustomTextBox/CustomTextBox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoCustomTextBox
+{
+    public class CustomTextBox : TextBox
+    {
+        public Color FocusColor { get; set; }       
+
+        public CustomTextBox()
+        {
+            this.Enter += CustomTextBox_Enter;
+            this.Leave += CustomTextBox_Leave;           
+        }
+
+        private void CustomTextBox_Enter(object sender, EventArgs e)
+        {
+            BackColor = FocusColor;
+        }
+
+        private void CustomTextBox_Leave(object sender, EventArgs e)
+        {
+            BackColor = Color.White;
+        }
+
+
+
+
+        
+    }
+}
